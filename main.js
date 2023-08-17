401476aafe6192270bc48bf392d7f6124f7acfe4
MatheusCode <97004563+Petrxz@users.noreply.github.com>
2023-08-17 06:42:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
     $('#cep').mask('00000-000');
 
+    $('#telefone').mask('00 00000-0000');
+    
     $('#formulario').submit(function(event) {
         event.preventDefult();
     })
